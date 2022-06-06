21b2413b5fb4e65695fd8e8b008bc181649f89ff
mcbrunette33 <mcbrunette33@gmail.com>
2022-06-06 15:03:47 -0700

    first push

---
+++ src/main/java/com/bycrptlogger/bycryptlogger/BycryptloggerApplication.java
@@ -0,0 +1,13 @@
+package com.bycrptlogger.bycryptlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BycryptloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BycryptloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bycrptlogger/bycryptlogger/BycryptloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycrptlogger.bycryptlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BycryptloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
